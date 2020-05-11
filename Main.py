135ff4b39148f044bf5fbb53657751de3d0916a7
DavidLake <d.lake@uq.net.au>
2020-05-12 04:59:10 +1000

    Scraping working and writing to output.csv

--- Main.py
+++ Main.py
@@ -8,22 +8,47 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(r"C:\Users\david\Documents\Personal\Projects\chromedriver.exe")
-driver.get("https://www.danmurphys.com.au/search?searchTerm=*&size=120&sort=Name")
+driver.get("https://www.danmurphys.com.au/search?searchTerm=*&size=30&sort=Name")
 
 time.sleep(6.5)
 
 links = []
+titles = []
+rawTitles = []
 
+
+count = 0
 URL = driver.find_elements_by_tag_name("a[href*=product")
 for u in URL:
     v = u.get_attribute('href')
-    if v not in links:
+    if v not in links and count < 30:
+        count += 1
         links.append(v)
 
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'lxml')
+data = [[] for j in range(len(links))]
 
-##with open("\output.csv", "w") as f:
-##    writer = csv.writer(f)
-    #for item_property_list in collected_items:
-    #    writer.writerow(item_property_list)
+c = 0
+d = 0
+for link in links:
+    driver.get(str(link))
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    title = soup.find_all("span", class_="item")
+    value = soup.find_all("span", class_="item_value")
+    for t in title:
+        p = t.text
+        rawTitles.append(p)
+        if p not in titles:
+            titles.append(p)
+            for n in data:
+                n.append("")
+    for v in value:
+        data[d][titles.index(rawTitles[c])] = v.text
+        c += 1
+    d += 1
+
+with open('output.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(titles)
+    for a in data:
+        writer.writerow(a)
