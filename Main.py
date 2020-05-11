213bcd0f74f85bb9c0b6ce302c71d92ab7185d75
DavidLake <d.lake@uq.net.au>
2020-05-11 22:06:02 +1000

    Beatiful soup added

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-python goes weeee
+import requests
+from bs4 import BeautifulSoup
+result = requests.get("https://www.danmurphys.com.au/search?searchTerm=*&sort=PriceDesc")
+#result = requests.get("https://www.google.com.au/")
+#print(result.status_code)
+#print(result.headers)
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+links = soup("a")
+print(links)
