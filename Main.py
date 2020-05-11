2dbf08a8509d73ac20d3e6e1d404b68aecbf18cc
DavidLake <d.lake@uq.net.au>
2020-05-11 23:53:09 +1000

    Working now but only for the header and footer

--- Main.py
+++ Main.py
@@ -1,10 +1,15 @@
 import requests
+import time
 from bs4 import BeautifulSoup
-result = requests.get("https://www.danmurphys.com.au/search?searchTerm=*&sort=PriceDesc")
-#result = requests.get("https://www.google.com.au/")
-#print(result.status_code)
-#print(result.headers)
-src = result.content
-soup = BeautifulSoup(src, 'lxml')
-links = soup("a")
-print(links)
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+driver = webdriver.Chrome(r"C:\Users\david\Documents\Personal\Projects\chromedriver.exe")
+driver.get("https://www.danmurphys.com.au/search?searchTerm=*")
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, 'lxml')
+
