8b6a3fe2285b72cb049104283cf3d90dba6a8a4c
DavidLake <d.lake@uq.net.au>
2020-05-12 01:23:43 +1000

    Now working for whole page

--- Main.py
+++ Main.py
@@ -9,7 +9,10 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(r"C:\Users\david\Documents\Personal\Projects\chromedriver.exe")
-driver.get("https://www.danmurphys.com.au/search?searchTerm=*")
+driver.get("https://www.danmurphys.com.au/search?searchTerm=*&size=120&sort=Name")
+time.sleep(6)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
-
+prices = soup.find_all('span', class_='value')
+for price in prices:
+    print(price.text)
