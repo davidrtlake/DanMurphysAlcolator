e8e598c6b1bad1bd006b02d155de2b9bd2ccfff4
DavidLake <d.lake@uq.net.au>
2020-05-12 03:04:37 +1000

    Created list of product urls

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-import requests
-import time
+import requests, time, csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -10,9 +9,21 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(r"C:\Users\david\Documents\Personal\Projects\chromedriver.exe")
 driver.get("https://www.danmurphys.com.au/search?searchTerm=*&size=120&sort=Name")
-time.sleep(6)
+
+time.sleep(6.5)
+
+links = []
+
+URL = driver.find_elements_by_tag_name("a[href*=product")
+for u in URL:
+    v = u.get_attribute('href')
+    if v not in links:
+        links.append(v)
+
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
-prices = soup.find_all('span', class_='value')
-for price in prices:
-    print(price.text)
+
+##with open("\output.csv", "w") as f:
+##    writer = csv.writer(f)
+    #for item_property_list in collected_items:
+    #    writer.writerow(item_property_list)
